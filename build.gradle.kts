5aa4e4a7f5addc690a829dfc2864b572b764cc0f
sad25kag <sad25kag@gmail.com>
2026-05-11 08:09:16 +0700

    Initial Anichin

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,14 +7,12 @@ buildscript {
     repositories {
         google()
         mavenCentral()
-        // Shitpack repo which contains our tools and dependencies
         maven("https://jitpack.io")
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.7.3")
-        // Cloudstream gradle plugin which makes everything work and builds plugins
-        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
+        classpath("com.android.tools.build:gradle:8.7.3") // Jangan diganti ke versi terbaru, karena ada masalah dengan versi terbaru
+        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")
     }
 }
@@ -27,6 +25,7 @@ allprojects {
     }
 }
 
+
 fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) = extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
 
 fun Project.android(configuration: BaseExtension.() -> Unit) = extensions.getByName<BaseExtension>("android").configuration()
@@ -37,18 +36,19 @@ subprojects {
     apply(plugin = "com.lagradost.cloudstream3.gradle")
 
     cloudstream {
-        // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
-        // you can modify it to use other git hosting services, like gitlab
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/user/repo")
-    }
+    setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/duro92/ExtCloud")
+    authors = listOf("duro92")
+        }
+
 
     android {
-        namespace = "recloudstream"
+        namespace = "com.excloud"
 
         defaultConfig {
             minSdk = 21
             compileSdkVersion(35)
             targetSdk = 35
+            
         }
 
         compileOptions {
@@ -56,9 +56,10 @@ subprojects {
             targetCompatibility = JavaVersion.VERSION_1_8
         }
 
+
         tasks.withType<KotlinJvmCompile> {
             compilerOptions {
-                jvmTarget.set(JvmTarget.JVM_1_8) // Required
+                jvmTarget.set(JvmTarget.JVM_1_8)
                 freeCompilerArgs.addAll(
                     "-Xno-call-assertions",
                     "-Xno-param-assertions",
@@ -69,19 +70,28 @@ subprojects {
     }
 
     dependencies {
+        val cloudstream by configurations
         val implementation by configurations
+        
+        // Cloudstream dependencies
+        cloudstream("com.lagradost:cloudstream3:pre-release")
 
-        implementation("com.github.recloudstream.cloudstream:library:-SNAPSHOT")
+        // Other dependencies
+        implementation(kotlin("stdlib")) // Untuk Kotlin Standard Library
+        implementation("com.github.Blatzar:NiceHttp:0.4.13") // Untuk HTTP requests
+        implementation("org.jsoup:jsoup:1.18.3") // Untuk parsing HTML
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.0") // JANGAN DIGANTI ke versi terbaru, karena ada masalah dengan versi terbaru
+        implementation("com.fasterxml.jackson.core:jackson-databind:2.16.0") // Untuk serialisasi/deserialisasi JSON
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.1") // Untuk coroutines di Android
+        implementation("com.faendir.rhino:rhino-android:1.6.0") // Untuk JavaScript engine
+        implementation("me.xdrop:fuzzywuzzy:1.4.0") // Untuk fuzzy matching
+        implementation("com.google.code.gson:gson:2.11.0") // Untuk serialisasi/deserialisasi JSON
+        implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3") // Untuk serialisasi/deserialisasi JSON
+        implementation("app.cash.quickjs:quickjs-android:0.9.2") // Untuk JavaScript engine
+        implementation("com.squareup.okhttp3:okhttp:4.12.0") // Untuk HTTP requests
+        implementation("androidx.core:core-ktx:1.16.0") // Untuk Log dan utilitas Android
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1") // Untuk coroutines
 
-        // These dependencies can include any of those which are added by the app,
-        // but you don't need to include any of them if you don't need them.
-        // https://github.com/recloudstream/cloudstream/blob/master/app/build.gradle.kts
-        implementation(kotlin("stdlib")) // Adds Standard Kotlin Features
-        implementation("com.github.Blatzar:NiceHttp:0.4.11") // HTTP Lib
-        implementation("org.jsoup:jsoup:1.18.3") // HTML Parser
-        // IMPORTANT: Do not bump Jackson above 2.13.1, as newer versions will
-        // break compatibility on older Android devices.
-        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1") // JSON Parser
     }
 }
 
