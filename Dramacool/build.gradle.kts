9a638de2783e064e65869b3b4e7a8f33cab377fc
sad25kag <sad25kag@gmail.com>
2026-05-12 11:39:06 +0700

    add

---
+++ Dramacool/build.gradle.kts
@@ -0,0 +1,20 @@
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.10")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
+}
+
+version = 2
+
+cloudstream {
+    language = "id"
+    description = "Asian Drama, Movies and Shows English Sub"
+    authors = listOf("BetbetMiro")
+    status = 1
+    tvTypes = listOf(
+        "AsianDrama",
+        "TvSeries",
+        "Movie",
+    )
+    iconUrl = "https://asianctv.net/favicon.ico"
+}
