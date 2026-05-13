f35cac29aa72bb56f855fe6cc2e07f687c11d98d
sad25kag <sad25kag@gmail.com>
2026-05-13 19:19:11 +0700

    Create kotlin/Yunshanid/YunshanidPlugin.kt

---
+++ Yunshanid/src/main/kotlin/Yunshanid/YunshanidPlugin.kt
@@ -0,0 +1,23 @@
+package Yunshanid
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import com.lagradost.cloudstream3.extractors.*
+import android.content.Context
+
+@CloudstreamPlugin
+class YunshanidPlugin : Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(YunshanidProvider())
+        
+        // Daftar Extractor populer untuk web streaming Indonesia
+        registerExtractorAPI(PixelDrain())
+        registerExtractorAPI(Gofile())
+        registerExtractorAPI(Krakenfiles())
+        registerExtractorAPI(Mediafire())
+        registerExtractorAPI(Mp4Upload())
+        registerExtractorAPI(FileMoon())
+        registerExtractorAPI(StreamWish())
+        registerExtractorAPI(VidHidePro())
+    }
+}
