5df1f683861037f81baf2cffb91bbaffd48878b4
sad25kag <sad25kag@gmail.com>
2026-05-13 20:35:42 +0700

    Update build.gradle.kts

--- Yunshanid/build.gradle.kts
+++ Yunshanid/build.gradle.kts
@@ -7,8 +7,5 @@ cloudstream {
     status = 1
     tvTypes = listOf("Anime", "Movie", "TvSeries")
     
-    // Tambahkan ini biar aplikasi gak bingung nyari pintu masuknya
-    pluginClass = "Yunshanid.YunshanidPlugin"
-    
     iconUrl = "https://yunshanid.site/favicon.ico"
 }
