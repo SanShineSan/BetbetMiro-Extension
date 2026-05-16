b2416dac71b91285caf547597d61f8d5ee0fad63
sad25kag <sad25kag@gmail.com>
2026-05-16 20:59:26 +0700

    add ReelShort extension using optimized JSON proxy API

---
+++ Reelshort/build.gradle.kts
@@ -0,0 +1,13 @@
+version = 1
+
+cloudstream {
+    description = "Nonton Koleksi Short Drama / Drama Pendek Subtitle Indonesia di ReelShort"
+    language    = "id"
+    authors     = listOf("BetbetMiro")
+    
+    status = 1
+    tvTypes = listOf("TvSeries", "AsianDrama")
+    iconUrl = "https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://reelshort.dramaview.web.id&size=%size%"
+
+    isCrossPlatform = true
+}
