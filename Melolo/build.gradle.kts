c5ccc5b7a28f5079359cf9a3eb8e7e8789285743
sad25kag <sad25kag@gmail.com>
2026-05-16 07:41:09 +0700

    add

---
+++ Melolo/build.gradle.kts
@@ -0,0 +1,15 @@
+// use an integer for version numbers
+version = 3
+
+cloudstream {
+    description = "😶 Melolo - Platform streaming Drama Asia (Short-form) dengan subtitle Indonesia!"
+    language = "id"
+    authors = listOf("BetbetMiro")
+
+    status = 1
+
+    tvTypes = listOf("TvSeries", "AsianDrama")
+    iconUrl = "https://www.google.com/s2/favicons?domain=tmthreader.com&sz=%size"
+
+    isCrossPlatform = false
+}
