6d91884b4a38ceb25734c47203fa1e190f12e8d6
sad25kag <sad25kag@gmail.com>
2026-06-24 17:18:05 +0700

    fix(moenime): restore Gradle build compatibility
    - Removed module-local Android/Kotlin plugin configuration that conflicts with root project setup
    - Dropped invalid CloudStream name assignment; module name remains provided by the project folder
    - Relied on root compilerOptions JVM 17 configuration instead of deprecated kotlinOptions.jvmTarget
    - Preserved Moenime metadata and bumped version to 2

--- Moenime/build.gradle.kts
+++ Moenime/build.gradle.kts
@@ -1,37 +1,10 @@
-plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
-    id("com.lagradost.cloudstream3.gradle")
-}
+version = 2
 
 cloudstream {
-    authors = listOf("BetbetMiro")
+    description = "Moenime"
     language = "id"
-    name = "Moenime"
-    version = 1
+    authors = listOf("BetbetMiro")
     status = 3 // Working
     tvTypes = listOf("Anime", "AnimeMovie", "OVA")
     iconUrl = "https://moenime.com/favicon.ico"
-}
-
-android {
-    namespace = "com.sad25kag.moenime"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 21
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-}
-
-dependencies {
-    implementation("com.lagradost.cloudstream3:lib:master-SNAPSHOT")
 }
