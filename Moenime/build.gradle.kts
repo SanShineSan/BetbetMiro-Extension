b270ec09e76c8daed70195b83e2b8c0b06128257
sad25kag <sad25kag@gmail.com>
2026-06-24 17:11:03 +0700

    git commit -m "fix(Moenime): perbaiki provider Moenime (update domain & build config)
    - Ganti mainUrl dari maonime.com ke moenime.com
    - Rename class, package, folder & metadata dari Maonime ke Moenime
    - Update build.gradle.kts dengan plugin cloudstream yang benar + konfigurasi standar
    - Sesuaikan parsing cards, episodes, search, dan Maodrive resolver
    - Fix struktur extension agar bisa di-build sukses di CloudStream"

--- Moenime/build.gradle.kts
+++ Moenime/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
-    id("com.lagradost.cloudstream3")
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("com.lagradost.cloudstream3.gradle")
 }
 
 cloudstream {
@@ -9,5 +11,27 @@ cloudstream {
     version = 1
     status = 3 // Working
     tvTypes = listOf("Anime", "AnimeMovie", "OVA")
-    iconUrl = "https://moenime.com/favicon.ico" // or appropriate
+    iconUrl = "https://moenime.com/favicon.ico"
+}
+
+android {
+    namespace = "com.sad25kag.moenime"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 21
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("com.lagradost.cloudstream3:lib:master-SNAPSHOT")
 }
